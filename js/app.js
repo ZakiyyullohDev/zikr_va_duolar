1db79e7b8571a64a4d5922095491ebd4f1222529
ZakiyyullohDev <141619922+ZakiyyullohDev@users.noreply.github.com>
2024-02-28 11:40:34 +0500

    Update Version v1.2.0 Alpha

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const dua = document.querySelectorAll('.dua')
+const counterBtn = document.querySelectorAll('#counterBtn')
 const copyDuaBtns = document.querySelectorAll('#copyDua');
 const duaNames = document.querySelectorAll('#duaName');
 const duaTexts = document.querySelectorAll('#duaText');
@@ -11,4 +13,21 @@ copyDuaBtns.forEach((btn, index) => {
         copyDuaFunc(duaNames[index], duaTexts[index]);
         alert("Nusxa olindi!");
     });
+});
+
+const counterFunc = (duas) => {
+    counterBtn.forEach((btn, index) => {
+        btn.addEventListener('click', () => {
+            let result = btn[index].textContent - 1
+            if (result === 0) {
+                duas.classList.add('display-none')
+            }
+        });
+    });
+}
+
+dua.forEach((duas) => {
+    duas.addEventListener('click', () => {
+        counterFunc(duas)
+    })
 });
