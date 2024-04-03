2df03bac494409c0e6b13306cf6d33131e3f3a6f
ZakiyyullohDev <141619922+ZakiyyullohDev@users.noreply.github.com>
2024-04-03 15:49:44 +0500

    Fix some bugs

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const dua = document.querySelectorAll('#dua')
-const copyDuaBtns = document.querySelectorAll('#copyDua');
-const duaNames = document.querySelectorAll('#duaName');
-const duaTexts = document.querySelectorAll('#duaText');
+const copyDuaBtns = document.querySelectorAll('#copyDua')
+const duaNames = document.querySelectorAll('#duaName')
+const duaTexts = document.querySelectorAll('#duaText')
 const oneDua = document.querySelectorAll('#counterBtn1')
 const threeDua = document.querySelectorAll('#counterBtn3')
 const sevenDua = document.querySelectorAll('#counterBtn7')
@@ -9,72 +9,82 @@ const tenDua = document.querySelectorAll('#counterBtn10')
 const ThousandDua = document.querySelectorAll('#counterBtn100')
 
 const copyDuaFunc = (duaName, duaText) => {
-    navigator.clipboard.writeText(`${duaName.textContent}: ${duaText.textContent}`);
+    navigator.clipboard.writeText(`${duaName.textContent}: ${duaText.textContent}`)
 }
 
 copyDuaBtns.forEach((btn, index) => {
     btn.addEventListener('click', () => {
-        copyDuaFunc(duaNames[index], duaTexts[index]);
+        copyDuaFunc(duaNames[index], duaTexts[index])
         alert("Nusxa olindi!");
     });
 });
 
 oneDua.forEach(duaOne => {
-    duaOne.addEventListener('click', ()=> {
+    duaOne.parentElement.parentElement.addEventListener('click', () => {
         let count = parseInt(duaOne.textContent);
         if (count > 0) {
             duaOne.textContent = count - 1;
         }
-        if (count <= 1) {
-            duaOne.textContent = 'Duo Tugadi'
+        if (count <= 1)
+        {
+            duaOne.parentElement.parentElement.classList.add('endDua')
+            duaOne.classList.add('display-none')
         }
     })
 });
 
 threeDua.forEach(duaThree => {
-    duaThree.addEventListener('click', ()=> {
+    duaThree.parentElement.parentElement.addEventListener('click', () => {
         let count = parseInt(duaThree.textContent);
         if (count > 0) {
             duaThree.textContent = count - 1;
         }
         if (count <= 1) {
-            duaThree.textContent = 'Duo Tugadi'
+            
+            duaThree.parentElement.parentElement.classList.add('endDua')
+            duaThree.classList.add('display-none')
         }
     })
 });
 
 sevenDua.forEach(duaSeven => {
-    duaSeven.addEventListener('click', ()=> {
+    duaSeven.parentElement.parentElement.addEventListener('click', () => {
         let count = parseInt(duaSeven.textContent);
         if (count > 0) {
             duaSeven.textContent = count - 1;
         }
         if (count <= 1) {
-            duaSeven.textContent = 'Duo Tugadi'
+            
+            duaSeven.parentElement.parentElement.classList.add('endDua')
+            duaSeven.classList.add('display-none')
         }
     })
 });
 
 tenDua.forEach(duaTen => {
-    duaTen.addEventListener('click', ()=> {
+    duaTen.parentElement.parentElement.addEventListener('click', () => {
         let count = parseInt(duaTen.textContent);
         if (count > 0) {
             duaTen.textContent = count - 1;
         }
-        if (count <= 1) {
-            duaTen.textContent = 'Duo Tugadi'
+        if (count <= 1)
+        {
+            duaTen.parentElement.parentElement.classList.add('endDua')
+            duaTen.classList.add('display-none')
         }
     })
 });
 
 ThousandDua.forEach(duaThousand => {
-    duaThousand.addEventListener('click', ()=> {
+    duaThousand.parentElement.parentElement.addEventListener('click', () => {
         let count = parseInt(duaThousand.textContent);
         if (count > 0) {
             duaThousand.textContent = count - 1;
         }
         if (count <= 1) {
-            duaThousand.textContent = 'Duo Tugadi'
+            
+            duaThousand.parentElement.parentElement.classList.add('endDua')
+            duaThousand.classList.add('display-none')
         }
     })
 });
